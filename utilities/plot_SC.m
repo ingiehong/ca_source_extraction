ba25a6e9d51618c7b5ec41b531abca16470b8475
Ingie Hong <ingiehong@jhmi.edu>
2016-05-17 12:37:31 -0400

    Updated plot_SC().

--- utilities/plot_SC.m
+++ utilities/plot_SC.m
@@ -1,24 +1,34 @@
 
 
 % Plot spatial component and S,C
-unit=zeros(d1,d2);
+unit=reshape(P.sn,d1,d2)/max(P.sn); %zeros(d1,d2);
+max_unit=reshape(P.sn,d1,d2)/max(P.sn); %zeros(d1,d2);
 
 % Open decent size figure
 figure
 scrsz = get(groot,'ScreenSize');
 set(gcf, 'Position',[50 50 scrsz(3)-100 scrsz(4)*2/3-150])
 pan2 = panel();
-pan2.pack('h', {1/4 []} )
+pan2.pack('h', {1/4 1/4 []} )
 
 for i=1:size(S_df,1)
     % Spatial component
     pan2(1).select()
-    unit=unit + reshape(A(:,i), d1, d2);
+    unit=reshape(A_or(:,i), d1, d2);
     imagesc(unit);
-    axis image; 
+    axis image; axis equal; axis tight; axis ij;  
+    title(['Unit ' num2str(i) ' / ' num2str(size(S_df,1))])
+    
+    % Maximal activation of Spatial component
+    pan2(2).select()
+    [~,max_i]=max(C_df(i,:));
+    max_unit=max_unit + reshape( A_or(:,i).* Yr(:,max_i)/max(A_or(:,i).* Yr(:,max_i)), d1, d2);
+    imagesc(max_unit);
+    axis image; axis equal; axis tight; axis ij; 
+    title(['Unit ' num2str(i) ' / ' num2str(size(S_df,1))])
     
     % S and C
-    pan2(2).select()
+    pan2(3).select()
     plot([S_df(i,:); C_df(i,:)]')
     pause
 end
